2d268d28570251d44aad099ba2de73f29fd02ea4
Sherman Hewitt <shewitt7@umiami.edu>
2016-01-06 18:21:47 -0500

    Added jumping div functionality. Need to look into horizontal smooth scroll for final product.

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict"
+
+$(document).ready(function() {
+
+  var golden = $(".golden"),
+      richt = $(".richt");
+
+  function changeCard(event) {
+    var card = $(this);
+
+    if (card.hasClass("golden")) {
+      card.css("left", "-9999em");
+      richt.css("left", "15%");
+    } else {
+      card.css("left", "9999em");
+      golden.css("left", "15%");
+    }
+
+  }
+
+  golden.on("click", changeCard);
+  richt.on("click", changeCard);
+
+})
